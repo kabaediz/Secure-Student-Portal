8eb8144ffea5616e7a40279b3d3b8866cf40610a
kabaediz <Ediz-Can.Kaba@Student.HTW-Berlin.de>
2026-05-07 11:15:18 +0200

    Änderungen und Anpassungen

---
+++ src/main/java/com/studentportal/SecureStudentPortalApplication.java
@@ -0,0 +1,13 @@
+package com.studentportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureStudentPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureStudentPortalApplication.class, args);
+	}
+
+}
